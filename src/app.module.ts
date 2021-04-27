8854f22da410a8d024fe3dd756afc0a7cbf0a36a
Kamil Mastalerz <kamil.mastalerz@netguru.com>
2021-04-27 16:00:07 +0200

    Add movies model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       debug: false,
-      playground: true,
-      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    MoviesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/movies/models/movie.model.ts
@@ -0,0 +1,13 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Movie {
+  @Field({ description: 'Movie Id' })
+  id: string;
+
+  @Field({ description: 'Movie title' })
+  title: string;
+
+  @Field({ description: 'description about movie', nullable: true })
+  description?: string;
+}
